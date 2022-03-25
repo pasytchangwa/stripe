7b84993c44618ecce1c7f3af52c2cbc2529b0ec8
sylvestre <pasytchangwa@gmail.com>
2022-03-25 11:56:28 +0200

    Add App.js and context API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
+import Sidebar from "./component/Sidebar";
+import Submenu from "./component/Submenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </>
   );
 }
 
