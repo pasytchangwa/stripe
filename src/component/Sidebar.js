d216914bf24482250051671a4050bd1f245cfa33
sylvestre <pasytchangwa@gmail.com>
2022-03-25 12:32:40 +0200

    Add sidebar codes

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { useGlobalContext } from './context';
+import sublinks from '../data';
+import {FaTimes} from 'react-icons/fa'
 
 const Sidebar = () => {
+  const {isSidebar, closeSidebar} = useGlobalContext();
+ 
+
   return (
-    <div>Sidebar</div>
+    <aside className={isSidebar ? 'sidebar-wrapper show' : 'sidebar-wraper'}>
+     <div className="sidebar">
+      <button className="close-btn" onClick={closeSidebar} typebutton>
+        <FaTimes />
+      </button>
+      <div className="sidebar-links">
+        {sublinks.map((item, index) => {
+          const {links, page} = item;
+
+          return (
+            <article key={index}>
+              <h4>{page}</h4>
+              <div className="sidebar-sublinks">
+                {links.map((link,index) => {
+                  const {label, icon, url} = link;
+
+                  return (
+                     <a key={index} href={url}>
+                      {icon}
+                      {label}
+                     </a>
+                  )
+                })}
+              </div>
+            </article>
+          )
+        })}
+      </div>
+     </div>
+    </aside>
   )
 }
 
