02e05bd1bd2c032ead768dc5baecc1bbf2d84c22
sylvestre <pasytchangwa@gmail.com>
2022-03-25 12:33:05 +0200

    Add Navbar code

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,8 +6,37 @@ import {FaBars} from 'react-icons/fa'
 const Navbar = () => {
   const {openSidebar, closeSubmenu, openSubmenu} = useGlobalContext();
   return (
-    <div>Navbar</div>
-  )
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='stripe' className='nav-logo' />
+          <button className='btn toggle-btn' type='button' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button type='button' className='link-btn'>
+              products
+            </button>
+            <li>
+              <button type='button' className='link-btn'>
+                developers
+              </button>
+            </li>
+            <li>
+              <button type='button' className='link-btn'>
+                company
+              </button>
+            </li>
+          </li>
+        </ul>
+        <button type='button' className='btn signin-btn'>
+          Sign in
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
